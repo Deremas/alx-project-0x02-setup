b7b146ad15e83956c7e5c417090fa2b67b21d81d
Dereje Masresha <derejemasresha27@gmail.com>
2025-09-25 18:28:22 +0300

    posts page done with posts fetching

--- alx-project-0x02/components/common/PostCard.tsx
+++ alx-project-0x02/components/common/PostCard.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { PostProps } from "@/interfaces";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
-const PostCard = (props: Props) => {
+const PostCard: React.FC<PostProps> = ({ userId, title, content }) => {
   return (
-    <div>PostCard</div>
-  )
-}
+    <>
+      <div className="border text-black rounded-lg shadow p-4 bg-white hover:shadow-md transition">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p className="text-gray-600 text-sm">User: {userId}</p>
+        <p className="mt-2">{content}</p>
+      </div>
+    </>
+  );
+};
 
-export default PostCard
+export default PostCard;
--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,13 +1,48 @@
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
-import React from "react";
+import { PostProps } from "@/interfaces";
+import React, { useEffect, useState } from "react";
 
-type Props = {};
+const posts = () => {
+  const [posts, setPosts] = useState<PostProps[]>([]);
 
-const posts = (props: Props) => {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const data = await response.json();
+        // Select only the first 15 posts
+        const first15Posts = data.slice(0, 15).map((post: any) => ({
+          userId: post.userId,
+          title: post.title,
+          content: post.body,
+        }));
+        setPosts(first15Posts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <Header />
-      <h1>Posts Page</h1>
+      <div className="p-8">
+        <h1 className="text-3xl font-bold text-center mb-6">Posts</h1>
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {posts.map((post, index) => (
+            <PostCard
+              key={index}
+              userId={post.userId}
+              title={post.title}
+              content={post.content}
+            />
+          ))}
+        </section>
+      </div>
     </>
   );
 };
